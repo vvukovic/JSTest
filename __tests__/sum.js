d7e2f5f8d25604ee160a78055fffb652fda1af07
Vedran Vukovic <vedran@outlook>
2020-06-01 18:28:43 +0200

    App init

---
+++ __tests__/sum.js
@@ -0,0 +1,11 @@
+const sum = require('../src/js/sum');
+
+test('tram11', () => {
+    expect(sum(1,1)).toBe(2)
+});
+
+// Result
+
+// test('tram11', () => {
+//     expect('1'+'1').toBe('11')
+// });
